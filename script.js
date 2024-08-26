598e37de8bbab2d2b84086d909fb038cab9a0f21
Gregory Burgess <gregoryburgess8498@gmail.com>
2024-08-26 11:24:52 -1000

    Updated message in hello_world website

--- script.js
+++ script.js
@@ -5,5 +5,5 @@ const button = document.getElementById('changeTextButton');
 // Add an event listener to the button
 button.addEventListener('click', function() {
     // Change the text of the greeting element
-    greetingElement.textContent = 'You clicked the button!';
+    greetingElement.textContent = 'So much it could burst my heart in two so she should kiss me!';
 });
